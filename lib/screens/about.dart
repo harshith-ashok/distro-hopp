4921d157fa737122d699653b5b54f5d1975cd395
Harshith Ashok <harshith.7k10@gmail.com>
2023-06-19 11:30:57 +0530

    Finished Games and UI

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
@@ -10,6 +11,61 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'About',
+          style: GoogleFonts.montserrat(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              backgroundColor: Colors.blue,
+              radius: 80,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Distro_Hopp',
+                style: GoogleFonts.shareTechMono(
+                  fontSize: 40,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'Tiers and Description of Distributions from It\'s FOSS and other popular websites.\nI do not own any content or sponsor any distributions\nthis web-app is simply a complicated tier list of popular distros',
+                    style: GoogleFonts.shareTechMono(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Text(
+                'Developed by Harshith Ashok with lots of love using Flutter for web',
+                style: GoogleFonts.shareTechMono(
+                  fontSize: 12,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
